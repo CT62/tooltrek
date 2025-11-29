ed1f052f198c69b8293836e7396da10006b35ee1
clive <clivethompson@gmail.com>
2025-11-29 22:58:51 +0000

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <body className="antialiased font-sans">
+    <html lang="en">
+      <body className="antialiased">
         {children}
       </body>
     </html>
